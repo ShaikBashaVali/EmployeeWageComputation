4f30fd5d27f038326b359c6bc6d7e4c9f5e5b0a6
BashaVali <bashavali222@gmail.com>
2023-02-18 07:07:59 +0530

    [Basha]Add:CalculateDailyWage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,20 +6,29 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
+            //local variables
+            int empHrs = 0;
+            int empWage = 0;
+            //constants
+            const int EMP_PER_HRS = 20;
             const int IS_FULL_TIME = 1;
-            Console.WriteLine("Welcome to the Employee Wage Computation\n");
+            Console.WriteLine("Welcome to the Employee Wage Computation");
 
-            //UC1- EmployeeAttendence
+            //UC2- Calculate Daily Employee Wage
             Random random = new Random();
-            int empAttendence = random.Next(0, 2);//0 or 1
+            int empAttendence = random.Next(0, 2);
             if (empAttendence == IS_FULL_TIME)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee is Present");
             }
             else
             {
+                empHrs = 0;
                 Console.WriteLine("Employee is Absent");
             }
+            empWage = empHrs * EMP_PER_HRS;
+            Console.WriteLine("Employee Daily Wage is :" + empWage);
             Console.ReadLine();
         }
     }
