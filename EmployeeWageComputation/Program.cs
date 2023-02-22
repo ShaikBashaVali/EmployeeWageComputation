1d415110a1d974f453a14fb3c71c1091eb219b9e
BashaVali <bashavali222@gmail.com>
2023-02-23 00:34:57 +0530

    [Basha]Add :TotalWageForEachCompany

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -3,21 +3,33 @@ using System.Diagnostics;
 
 namespace EmployeeWageComputation
 {
-    public class Program
+    public class EmpWageBuilder
     {
         //constants
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public static void ComputeEmpWage(string company, int empPerHrs, int maxWorkingDays, int maxWorkingHrs)
+        public string company;
+        public int maxWorkingDays, maxWorkingHrs, empPerHrs;
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="company"></param>
+        /// <param name="empPerHrs"></param>
+        /// <param name="maxWorkingDays"></param>
+        /// <param name="maxWorkingHrs"></param>
+        public EmpWageBuilder(string company, int empPerHrs, int maxWorkingDays, int maxWorkingHrs)
+        {
+            this.company = company;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHrs = maxWorkingHrs;
+            this.empPerHrs = empPerHrs;
+        }
+        public void ComputeEmpWage()
         {
             //local variables
-            int empHrs = 0;
-            int empWage = 0;
-            int day = 1;
-            int totalEmpWage = 0;
-            int totalEmpHrs = 0;
+            int empHrs = 0, empWage = 0, day = 1, totalEmpWage = 0, totalEmpHrs = 0;
 
-            //UC8- Compute Employee Wage for Multiple Company
+            //UC9- Save the Total Wage for Each Company
             Random random = new Random();
             while (day <= maxWorkingDays && totalEmpHrs <= maxWorkingHrs)
             {
@@ -43,11 +55,13 @@ namespace EmployeeWageComputation
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Employee Wage Computation \n........................................\n");
-            ComputeEmpWage("TCS", 25, 10, 20);
-            ComputeEmpWage("Capgemini", 25, 20, 30);
-            ComputeEmpWage("Wipro", 35, 30, 45);
-            Console.ReadLine();
+            Console.WriteLine("Welcome to the Employee Wage Computation\n.........................................\n");
+            EmpWageBuilder tcs = new EmpWageBuilder("TCS", 25, 10, 20);
+            tcs.ComputeEmpWage();
+            EmpWageBuilder capgemini = new EmpWageBuilder("Capgemini", 25, 20, 30);
+            capgemini.ComputeEmpWage();
+            EmpWageBuilder wipro = new EmpWageBuilder("Wipro", 35, 30, 45);
+            wipro.ComputeEmpWage();
         }
     }
 }
